47a8e95f991df84bca5f5c3a48d0188779a1d9d8
Gilbert Molina <gmolinac@outlook.com>
2020-04-27 20:15:58 -0600

    Adding build folder

---
+++ build/precache-manifest.5d01c4d377ea0abbb24ba703dfa2112b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20f4dba5da9d8963a411c911e4d60b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26f933d61f07e5226169",
+    "url": "/static/css/main.26757dd9.chunk.css"
+  },
+  {
+    "revision": "d112e5bc9e468e67d7cd",
+    "url": "/static/js/2.6e93cc4a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.6e93cc4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26f933d61f07e5226169",
+    "url": "/static/js/main.4c182df6.chunk.js"
+  },
+  {
+    "revision": "71da307b76086a716d6b",
+    "url": "/static/js/runtime-main.3eec8023.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
